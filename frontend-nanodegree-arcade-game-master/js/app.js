222adcb555855e1c2e8b6889606581951d844cd6
Kevin <kevin.n.glick@gmail.com>
2015-07-08 15:02:29 +0300

    key modifications
    grab key function
    render key
    key visibility

--- frontend-nanodegree-arcade-game-master/js/app.js
+++ frontend-nanodegree-arcade-game-master/js/app.js
@@ -94,6 +94,19 @@ var Key = function() {
     this.y = 41.5;
 }
 
+ Key.prototype.update = function() {
+
+    var grabKey = Math.abs(player.x - this.x);
+    if (grabKey < 50.5 && this.y === player.y) {
+        this.sprite = display: none;
+        alert("You've got the key! Now open the gate!");
+    };
+}
+
+Key.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
