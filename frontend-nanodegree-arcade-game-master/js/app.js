7f0e9bbd287ad0d71ed8bdbd888ed471107bbec8
Kevin <kevin.n.glick@gmail.com>
2015-07-08 18:54:15 +0300

    change key start location

--- frontend-nanodegree-arcade-game-master/js/app.js
+++ frontend-nanodegree-arcade-game-master/js/app.js
@@ -91,7 +91,7 @@ Player.prototype.handleInput = function(keyPress) {
 var Key = function() {
     this.sprite = 'images/Key.png';
     this.x = 202;
-    this.y = 41.5;
+    this.y = 62.5;
 }
 
 Key.prototype.update = function(dt) {
